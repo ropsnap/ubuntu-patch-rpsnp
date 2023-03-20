77eeba95585d15113596defed97694faf3c46bb3
ropsnap <ropsnap9@gmail.com>
2023-03-20 05:53:37 -0300

    Update 1-cash-factory.sh

--- on-multiplass-instance/1-cash-factory.sh
+++ on-multiplass-instance/1-cash-factory.sh
@@ -4,7 +4,7 @@ clear;
 
 echo;
 echo ' -------------- Patch Script → cash-factory.sh -----------------';
-echo ' → This script setups CashFactory-lts project on vmc';
+echo ' → This script setups CashFactory-rpsnp project on vmc';
 echo ' ---------------------------------------------------------------';
 
 sleep 3
@@ -20,12 +20,12 @@ fi
 
 # thowww ------------------------------------------------------------- 1
 echo;
-echo ' [1] Downloading custom CashFactory-lts -------------------------';
+echo ' [1] Downloading custom CashFactory-rpsnp -------------------------';
 echo;
 
 sleep 1;
 
-cd && git clone https://github.com/ropsnap/CashFactory-lts;
+cd $HOME && git clone https://github.com/ropsnap/CashFactory-rpsnp;
 
 # bef-last ----------------------------------------------------------- 2
 echo;
@@ -34,23 +34,22 @@ echo;
 
 sleep 9
 
-bash $HOME/CashFactory-lts/setup.sh;
+bash $HOME/CashFactory-rpsnp/setup.sh;
 
 # -------------------------------------------------------------------- 3
 echo;
-echo ' [3] Openning default .env for edit or confirm -----------------';
+echo ' [3] Opening default .env for edit or confirm ------------------';
 echo;
 
 sleep 3
 
-cd $HOME/CashFactory-lts/;
-nano $HOME/CashFactory-lts/.env;
+cd $HOME/CashFactory-rpsnp/;
+nano $HOME/CashFactory-rpsnp/.env;
 
 # pre-last ----------------------------------------------------------- 5
 echo ' ✓ Setup is right done -----------------------------------------';
 echo;
-echo ' → Rununnin project  → cd ~/CashFactory-lts'
-echo;
+echo ' → Running project  → cd ~/CashFactory-rpsnp'
 echo ' → Turn on by   → sudo docker-compose up -d';
 echo ' → Turn off by  → sudo docker-compose down';
 echo
@@ -59,12 +58,12 @@ sleep 6
 
 # last --------------------------------------------------------------- 0
 echo
-echo ' ...starting CashFactory-lts.'
+echo ' ...starting CashFactory-rpsnp.'
 echo
 
 sleep 3
 
-cd ~/CashFactory-lts
+cd ~/CashFactory-rpsnp
 sudo docker-compose up -d;
 echo;
 
